1ae6cbfad7ebde17d1b83c7904354c178e4024ff
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-13 23:08:00 -0800

    create reset button event listener, track state of number of resets, display state to users

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const totalDisplay = document.getElementById('totals-element');
 const resultDisplay = document.getElementById('result-element');
 const submitButton = document.getElementById('submit-element');
 const resetButton = document.getElementById('reset-element');
+const resetCount = document.getElementById('reset-count');
 
 // initialize state
 let currentWins = 0;
 let totalMatches = 0;
+let reset = 0;
 
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
@@ -38,4 +40,15 @@ submitButton.addEventListener('click', () => {
         lossDisplay.textContent = totalMatches - currentWins;
         resultDisplay.textContent = 'You Lost!';
     }
+});
+
+resetButton.addEventListener('click', () => {
+    reset++;
+    resetCount.textContent = reset;
+    totalMatches = 0;
+    currentWins = 0;
+    winsDisplay.textContent = currentWins;
+    lossDisplay.textContent = totalMatches - currentWins;
+    totalDisplay.textContent = totalMatches;
+    resultDisplay.textContent = '';
 });
