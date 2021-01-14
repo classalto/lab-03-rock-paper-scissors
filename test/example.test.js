150e211184820bf24e86e3d461c7bf73a32094ed
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-13 21:37:21 -0800

    create unit tests for doesUserWin function

--- test/example.test.js
+++ test/example.test.js
@@ -2,7 +2,7 @@
 import { convertComputerInt, doesUserWin } from '../utils.js';
 
 const test = QUnit.test;
-
+// computer throw
 test('should return rock as a string when given 0', (expect) => {
     //Arrange
     // Set up your arguments and expectations
@@ -37,6 +37,34 @@ test('should return scissors when given 2', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = convertComputerInt(2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+// does user win
+test('should return true when given  rock , scissors', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = doesUserWin('rock', 'scissors');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('should return false when given paper , scissors', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = doesUserWin('paper', 'scissors');
 
     //Expect
     // Make assertions about what is expected versus the actual result
