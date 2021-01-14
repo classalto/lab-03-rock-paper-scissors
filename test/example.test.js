c86bd44f58e1b4bd45f50bf4b2d7436582c72d76
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-13 21:31:47 -0800

    create three unit tests for computer throw function, all passing

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,42 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { convertComputerInt, doesUserWin } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('should return rock as a string when given 0', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 'rock';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = convertComputerInt(0);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('should return paper when given 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'paper';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = convertComputerInt(1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('should return scissors when given 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'scissors';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = convertComputerInt(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
